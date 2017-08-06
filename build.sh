0112d89bf5e98fea55d332e5293f5517253b29af
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-06 08:41:38 -0400

    Cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 rm -dr dist/LiveMeToo*
-
 electron-packager src --out=dist --platform=win32  --arch=x64  --version-string.CompanyName='The Coder' --version-string.LegalCopyright='Copyright (C) 2017 by The Coder' --version-string.FileDescription=$npm_package_productName --version-string.OriginalFilename='LiveMeTools.exe' --version-string.InternalName=$npm_package_productName --version-string.ProductName=$npm_package_productName --version-string.ProductVersion=$npm_package_version --asar --icon=logo.ico --overwrite --prune=true 
 electron-packager src --out=dist --platform=win32  --arch=ia32 --version-string.CompanyName='The Coder' --version-string.LegalCopyright='Copyright (C) 2017 by The Coder' --version-string.FileDescription=$npm_package_productName --version-string.OriginalFilename='LiveMeTools.exe' --version-string.InternalName=$npm_package_productName --version-string.ProductName=$npm_package_productName --version-string.ProductVersion=$npm_package_version --asar --icon=logo.ico --overwrite --prune=true 
 electron-packager src --out=dist --platform=linux  --arch=x64  --icon=assets/icons/png/appicon.png --prune=true -asar --overwrite
