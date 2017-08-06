50dad8684dcfa3592f191def9f9314571ae42330
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-06 13:26:13 -0400

    Added version writer

--- src/lmt/javascript/livemetools.js
+++ src/lmt/javascript/livemetools.js
@@ -14,12 +14,14 @@
 
 */
 
-const electron = require('electron'), ipc = electron.ipcRenderer, remote = electron.remote, BrowserWindow = remote.BrowserWindow;
-const Menu = remote.Menu, lmt = require('./livemetoolsmodule');
+const {app, ipcRenderer, Menu, browserWindow, remote } = require('electron'), ipc = ipcRenderer;
+const lmt = require('./livemetoolsmodule');
  
 var isSearching = false;
 
 $(function(){
+
+	$('header h1 small').html(remote.app.getVersion());
 
 	onTypeChange();
 
