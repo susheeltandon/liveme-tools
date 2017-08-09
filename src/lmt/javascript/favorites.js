8032113a09a833f2f7d2bae5cf016296f273e02a
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-09 13:19:15 -0400

    Typo fix

--- src/lmt/javascript/favorites.js
+++ src/lmt/javascript/favorites.js
@@ -13,13 +13,13 @@ $(function(){
 
 		$('#small_user_list').html('');
 		if (favorites_list.length < 1) {
-			$('#small_user_list').html('<p style="padding-top: 260px; text-align: center; font-style: italic; color: rgba(255,255,255,0.3)">Your list is empty.</p>');
+			$('#small_user_list').html('<p style="padding-top: 260px; text-align: center; font-style: italic; color: rgba(255,255,255,0.4)">Your list is empty.</p>');
 		} else {
 			for (i = 0; i < favorites_list.length; i++) {
 				if (typeof (favorites_list[i].uid) != 'undefined') {
 					var t = '<div title="Click to view user\'s videos." class="user_entry small clickable ' + favorites_list[i].sex + '" onClick="getVideos(\'' + favorites_list[i].uid + '\')"><img class="avatar" src="' + favorites_list[i].face + '" onerror="this.src=\'images/blank.png\'"><h4>' + favorites_list[i].nickname + '</h4></div>';
 					$("#small_user_list").append(t);
-					clean_list.putsh(favorites_list[i]);
+					clean_list.push(favorites_list[i]);
 				} 
 			}
 			fs.writeFile(remote.app.getPath('appData') + '/' + remote.app.getName() + '/favorites.json', JSON.stringify(clean_list), 'utf8', function() {} );
