7155ac7c151f00ee3aeb8f526805279de899f26c
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-09 10:01:06 -0400

    Issue #13

--- src/lmt/javascript/livemetools.js
+++ src/lmt/javascript/livemetools.js
@@ -231,6 +231,7 @@ function beginSearch2() {
 			setTimeout(function(){
 				beginSearch2();
 			}, 100);
+
 		} else if (t[t.length-1].indexOf('yolo') > -1) {
 			var a=t[t.length - 1].split('-');
 			$('#type').val('video-lookup');
@@ -239,16 +240,34 @@ function beginSearch2() {
 				beginSearch2();
 			}, 100);
 			
-		} else if (t[t.length-1].indexOf('/liveme/') > -1) {
-			var a=t[t.length - 1].split('.');
-			q = a[0];
-		} else if (t[t.length - 1].indexOf('videoid') > -1) {
+		} else if (u.indexOf('videoid') > -1) {
 			var a=t[t.length - 1].split('?'),b=a[1].split('&');
 			console.log(a);
 			for (i = 0; i < b.length; i++) {
 				if (b[i].indexOf('videoid') > -1) {
 					var c=b[i].split('=');
-					q = c[1];
+					
+					$('#type').val('video-lookup');
+					$('#query').val(c[1]);
+					setTimeout(function(){
+						beginSearch2();
+					}, 100);
+
+				}
+			}
+		} else if (u.indexOf('userid') > -1) {
+			var a=t[t.length - 1].split('?'),b=a[1].split('&');
+			console.log(a);
+			for (i = 0; i < b.length; i++) {
+				if (b[i].indexOf('userid') > -1) {
+					var c=b[i].split('=');
+					
+					$('#type').val('user-lookup');
+					$('#query').val(c[1]);
+					setTimeout(function(){
+						beginSearch2();
+					}, 100);
+
 				}
 			}
 		} else {
