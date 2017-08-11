14b2584a3536c9673d3683c18d0092a6423dca78
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-11 18:45:26 -0400

    Added new options

--- src/lmt/javascript/settings.js
+++ src/lmt/javascript/settings.js
@@ -1,18 +1,43 @@
-const appSettings = require('electron').remote.require('electron-settings');
+const {electron, remote} = require('electron'), appSettings = remote.require('electron-settings');
+const path = require('path');
 
 $(function() {
-	getSettings();
+
+	if (appSettings.has('downloads.directory') == false) {
+		appSettings.set('downloads', {
+			directory : path.join(remote.app.getPath('home'), 'Downloads'),
+			filemode: 0,
+			filetemplate: '',
+			history: true,
+			concurrent: 1
+		});
+		
+	}
+
+	setTimeout(function(){
+		$('#download_folder').val(appSettings.get('downloads.directory'));
+		$('#filemode').val(appSettings.get('downloads.filemode'));
+
+	}, 250);
 });
 
 function closeWindow() {
 	window.close();
 }
 
-function getSettings() {
-	$('#download_folder').val(appSettings.get('downloader.directory'));
+function saveSettings() {
+	appSettings.set('downloads', { directory: $('#download_folder').val() });
+	closeWindow();
 }
 
-function setSettings() {
-	appSettings.set('downloader', { directory: $('#download_folder').val() });
-	closeWindow();
+
+
+function SetDownloadPath() {
+	var dir_path = remote.dialog.showOpenDialog({
+		properties: ['openDirectory']
+	});
+	if (typeof (dir_path) != 'undefined') {
+		appSettings.set('downloads', { directory: dir_path });
+		$('#download_folder').val(dir_path);
+	}
 }
