88228e9881763cafa76527a8bfb2c16912261b91
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-11 17:18:52 -0400

    Cleanup

--- src/lmt/javascript/favorites.js
+++ src/lmt/javascript/favorites.js
@@ -1,11 +1,11 @@
-const { remote, ipcRenderer } = require('electron').remote, app = remote.app, Favorites = require('./module/favorites');
+const { remote, ipcRenderer } = require('electron'), Favorites = require('./module/favorites');
 
 var last_change = 0;
 
 $(function(){
 	setInterval(function(){
 		var lc = Favorites.lastChange();
-		if ((last_change - lc) > 300) {
+		if ((lc - last_change) > 300) {
 			Favorites.load(function(e){
 				for (i = 0; i < e.length; i++) {
 					var t = '<div title="Click to view user\'s videos." class="user_entry small clickable ' + e[i].sex + '" onClick="getVideos(\'' + e[i].uid + '\')"><img class="avatar" src="' + e[i].face + '" onerror="this.src=\'images/blank.png\'"><h4>' + e[i].nickname + '</h4></div>';
