ba7f68bb4dcbbdaea53623288d144e63965755a8
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-13 08:00:52 -0400

    Cleanup

--- src/lmt/javascript/favorites.js
+++ src/lmt/javascript/favorites.js
@@ -10,13 +10,17 @@ $(function(){
 			Favorites.recall(function(e){
 				$('#small_user_list').html('');
 				for (i = 0; i < e.length; i++) {
-					var t = '<div title="Click to view user\'s videos." class="user_entry small clickable ' + e[i].sex + '" onClick="getVideos(\'' + e[i].uid + '\')"><img class="avatar" src="' + e[i].face + '" onerror="this.src=\'images/blank.png\'"><h4>' + e[i].nickname + '</h4></div>';
-					$("#small_user_list").append(t);
+					$("#small_user_list").append(`
+						<div title="Click to view user's videos." class="user_entry small clickable ${e[i].sex}" onClick="getVideos('${e[i].uid}')">
+							<img class="avatar" src="${e[i].face}" onerror="this.src='images/blank.png'">
+							<h4>${e[i].nickname}</h4>
+						</div>
+					`);
 				}
 			});
 			last_change = Favorites.lastChange();
 		}
-	}, 1000);
+	}, 500);
 });
 
 function closeWindow() { window.close(); }
