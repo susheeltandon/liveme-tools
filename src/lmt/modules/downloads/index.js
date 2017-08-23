66f9e8630e01474680b8b52d42b4f675b58b7f22
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-22 21:11:18 -0400

    Add error'd files back to end of the queue for retry later

--- src/lmt/modules/downloads/index.js
+++ src/lmt/modules/downloads/index.js
@@ -207,6 +207,7 @@ function processItem(item) {
                     resolve();
                 },
                 on_error: function(e) {
+                    download_queue.push(item);
                     ipcRenderer.send('download-error', { id: item.video.id });
                     resolve();
                 },
@@ -238,6 +239,7 @@ function processItem(item) {
                     ipcRenderer.send('download-start', { id: item.video.id, value: item.video.url });
                 })
                 .on('error', function(err, stdout, etderr) {
+                    download_queue.push(item);
                     ipcRenderer.send('download-error', { id: item.video.id });
                     resolve();
                 })
