a21aafd7aa78d59f781efd0eb2a2d5bfe0f8217c
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-31 09:21:01 -0400

    Updated layout of list

--- src/lmt/javascript/favorites.js
+++ src/lmt/javascript/favorites.js
@@ -6,17 +6,26 @@ $(function(){
 	}, 250);
 
 	ipcRenderer.on('favorites-refresh' , function(event , data) { 
-		Favorites.recall(function(e){
-			$('#small_user_list').empty();
-			for (i = 0; i < e.length; i++) {
+		$('#small_user_list').empty();
+		for (i = 0; i < data.length; i++) {
+			if (typeof data[i].stars == 'undefined') {
 				$("#small_user_list").append(`
-					<div title="Click to view user's videos." class="user_entry small clickable ${e[i].sex}" onClick="getVideos('${e[i].uid}')">
-						<img class="avatar" src="${e[i].face}" onerror="this.src='images/blank.png'">
-						<h4>${e[i].nickname}</h4>
+					<div title="Click to view user's videos." class="user_entry small clickable ${data[i].sex}" onClick="getVideos('${data[i].uid}')">
+						<img class="avatar" src="${data[i].face}" onerror="this.src='images/blank.png'">
+						<h4>${data[i].nickname}</h4>
+					</div>
+				`);
+			} else {
+				$("#small_user_list").append(`
+					<div title="Click to view user's videos." class="user_entry small clickable ${data[i].sex}" onClick="getVideos('${data[i].uid}')">
+						<img class="avatar" src="${data[i].face}" onerror="this.src='images/blank.png'">
+						<h4 class="nickname">${data[i].nickname}</h4>
+						<h4 class="usign">${data[i].usign}</h4>
+						<h5>${data[i].video_count}</h5>
 					</div>
 				`);
 			}
-		});
+		}
 	});
 
 
