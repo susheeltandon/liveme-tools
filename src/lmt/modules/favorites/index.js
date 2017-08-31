461d83ec1109d2122ac26dd48f86ef33bd743a60
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-08-31 09:20:44 -0400

    Updated to support new interfacing and data structure

--- src/lmt/modules/favorites/index.js
+++ src/lmt/modules/favorites/index.js
@@ -3,7 +3,7 @@
 */
 "use strict";
 
-const	fs = require('fs'), {remote, ipcRenderer} = require('electron'), path = require('path');
+const	fs = require('fs'), {remote, ipcRenderer} = require('electron'), path = require('path'), axios = require('axios');
 
 var fav_list = [], last_change = 0, is_saved = false, index = 0;
 
@@ -11,8 +11,9 @@ module.exports = {
 
 	add : function(e) {
 		fav_list.push(e);
-		ipcRenderer.send('favorites-refresh');
-		write_to_file(false);		
+		update_single_user(fav_list.length - 1);
+		ipcRenderer.send('favorites-refresh', fav_list);
+		write_to_file();		
 	},
 
 	remove: function(e) {
@@ -22,13 +23,13 @@ module.exports = {
 				fav_list.splice(i, 1);
 			}
 		}
-		ipcRenderer.send('favorites-refresh');
-		write_to_file(false);
+		ipcRenderer.send('favorites-refresh', fav_list);
+		write_to_file();
 	},
 
 	save: function() {
-		ipcRenderer.send('favorites-refresh');
-		write_to_file(false);
+		ipcRenderer.send('favorites-refresh', fav_list);
+		write_to_file();
 	},
 
 	recall : function(cb) { 
@@ -36,7 +37,20 @@ module.exports = {
 			if (err) {
 				fav_list = [];
 			} else {
-				fav_list = JSON.parse(data);
+				var i, j = JSON.parse(data);
+				for (i = 0; i < j.length; i++) {
+					fav_list.push({
+						'uid' : j[i].uid,
+						'face' : j[i].face,
+						'nickname' : j[i].nickname,
+						'sex' : j[i].sex,
+						'level' : j[i].level,
+						'video_count' : j[i].video_count,
+						'usign' : j[i].usign,
+						'stars' : j[i].stars
+					})
+				}
+
 				last_change = new Date().getTime() / 1000;
 				cb(fav_list);
 			}
@@ -48,8 +62,22 @@ module.exports = {
 			if (err) {
 				fav_list = [];
 			} else {
+				var i, j = JSON.parse(data);
+				for (i = 0; i < j.length; i++) {
+					fav_list.push({
+						'uid' : j[i].uid,
+						'face' : j[i].face,
+						'nickname' : j[i].nickname,
+						'sex' : j[i].sex,
+						'level' : j[i].level,
+						'video_count' : j[i].video_count,
+						'usign' : j[i].usign,
+						'stars' : j[i].stars
+					})
+				}
+
 				fav_list = JSON.parse(data);
-				ipcRenderer.send('favorites-refresh');
+				ipcRenderer.send('favorites-refresh', fav_list);
 			}
 		});
 
@@ -62,13 +90,11 @@ module.exports = {
 		return false;
 	},
 
-	tick : function() {	write_to_file(false); },
+	tick : function() { },
 	forceSave : function() { write_to_file(true); },
 	update: function() {
 		index = 0;
-		setTimeout(function(){
-			update_favorites_list();
-		}, 25);
+		update_favorites_list();
 	}
 }
 
@@ -76,8 +102,7 @@ function write_to_file() {
 	var ti = new Date().getTime() / 1000;
 	last_change = ti;
 
-	fs.writeFile(path.join(remote.app.getPath('appData'), remote.app.getName(), 'favorites.json'), JSON.stringify(fav_list), 'utf8', function(){		
-	});
+	fs.writeFile(path.join(remote.app.getPath('appData'), remote.app.getName(), 'favorites.json'), JSON.stringify(fav_list, null, 2), function(){ });
 }
 
 function read_from_file(cb) {
@@ -93,43 +118,76 @@ function read_from_file(cb) {
 
 }
 
+function update_single_user(index) {
+
+	axios.get('http://live.ksmobile.net/user/getinfo',{
+		params: { 
+			userid: fav_list[index].uid 
+		}
+	}).then(function(resp) {
+		var j = resp.data.data.user;
+
+		if (resp.data.status == 200) {
+			fav_list[index].face = j.user_info.face;
+			fav_list[index].nickname = j.user_info.nickname;
+			fav_list[index].usign = j.user_info.usign;
+			fav_list[index].level = j.user_info.level;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].currency = j.user_info.currency;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].sex = j.user_info.sex > -1 ? ( j.user_info.sex > 0 ? 'male' : 'female') : '';
+			fav_list[index].video_count = j.count_info.video_count;
+		} 
+
+		fs.writeFile(path.join(remote.app.getPath('appData'), remote.app.getName(), 'favorites.json'), JSON.stringify(fav_list, null, 2), function(){
+			update_favorites_list();
+			ipcRenderer.send('favorites-refresh', fav_list);
+		});
+
+	}).catch(function(err){
+		console.log('Error during favorites list update: ' + err);
+	});
+}
+
+
 function update_favorites_list() {
 
-	$.ajax({
-		url: 'http://live.ksmobile.net/user/getinfo',
-		data: {
-			userid: fav_list[index].uid
-		},
-		cache: false,
-		type: "GET",
-		dataType: "json",
-		timeout: 15000,
-		error: function(e){
-			callback_holder(return_data);
-		},
-		success: function(e) {
-
-			if (e.status != 500) {
-				fav_list[index].face = e.data.user.user_info.face;
-				fav_list[index].nickname = e.data.user.user_info.nickname;
-				if (index%9 == 0) {
-					ipcRenderer.send('favorites-refresh');
-				}
-			} else {
-				console.log(e);
-			}
-			
-			index++;
-			if (index < fav_list.length) {
-				setTimeout(function(){
-					update_favorites_list();
-				}, 50);
-			} else {
-				write_to_file();
-				ipcRenderer.send('favorites-refresh');
-			}
-
+	axios.get('http://live.ksmobile.net/user/getinfo',{
+		params: { 
+			userid: fav_list[index].uid 
 		}
-	});
+	}).then(function(resp) {
+		var j = resp.data.data.user;
 
+		if (resp.data.status == 200) {
+			fav_list[index].face = j.user_info.face;
+			fav_list[index].nickname = j.user_info.nickname;
+			fav_list[index].usign = j.user_info.usign;
+			fav_list[index].level = j.user_info.level;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].currency = j.user_info.currency;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].stars = j.user_info.star;
+			fav_list[index].sex = j.user_info.sex > -1 ? ( j.user_info.sex > 0 ? 'male' : 'female') : '';
+			fav_list[index].video_count = j.count_info.video_count;
+		} 
+
+		if (index%3==2) {
+			ipcRenderer.send('favorites-refresh', fav_list);
+		}
+		index++;
+
+		fs.writeFile(path.join(remote.app.getPath('appData'), remote.app.getName(), 'favorites.json'), JSON.stringify(fav_list, null, 2), function(){
+			if (index < fav_list.length) {
+				update_favorites_list();
+				ipcRenderer.send('favorites-refresh', fav_list);
+			}
+		});
+
+	}).catch(function(err){
+		console.log('Error during favorites list update: ' + err);
+	});
 }
