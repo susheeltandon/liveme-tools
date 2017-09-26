cfa94142fa43b247185659e577f38ec595a74f2f
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-09-26 13:49:56 -0400

    Added context menus to text fields

--- src/lmt/javascript/livemetools.js
+++ src/lmt/javascript/livemetools.js
@@ -67,6 +67,46 @@ $(function(){
 	});
 
 
+	const InputMenu = remote.Menu.buildFromTemplate([{
+	        label: 'Undo',
+	        role: 'undo',
+	    }, {
+	        label: 'Redo',
+	        role: 'redo',
+	    }, {
+	        type: 'separator',
+	    }, {
+	        label: 'Cut',
+	        role: 'cut',
+	    }, {
+	        label: 'Copy',
+	        role: 'copy',
+	    }, {
+	        label: 'Paste',
+	        role: 'paste',
+	    }, {
+	        type: 'separator',
+	    }, {
+	        label: 'Select all',
+	        role: 'selectall',
+	    },
+	]);
+
+	document.body.addEventListener('contextmenu', (e) => {
+	    e.preventDefault();
+	    e.stopPropagation();
+
+	    let node = e.target;
+
+	    while (node) {
+	        if (node.nodeName.match(/^(input|textarea)$/i) || node.isContentEditable) {
+	            InputMenu.popup(remote.getCurrentWindow());
+	            break;
+	        }
+	        node = node.parentNode;
+	    }
+	});
+
 });
 
 function copyToClipboard(i) { clipboard.writeText(i); }
