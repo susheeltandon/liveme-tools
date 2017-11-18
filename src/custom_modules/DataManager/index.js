285abaa48f1a8b58f1d38c0d43219776b6f82e9f
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-11-18 15:07:05 -0500

    Added code

--- src/custom_modules/DataManager/index.js
+++ src/custom_modules/DataManager/index.js
@@ -7,10 +7,13 @@
 const 	low = require('lowdb'), 
 		FileSync = require('lowdb/adapters/FileSync'),
 		path = require('path'),
-		{ remote } = require('electron')
-		adapter = new FileSync(path.join(remote.app.getPath('appData'), remote.app.getName(), 'livemetools_db.json')),
+		events = require('events'),
+		axios = require('axios'),
+		{ app } = require('electron'),
+		adapter = new FileSync(path.join(app.getPath('appData'), app.getName(), 'livemetools_db.json')),
 		db = low(adapter);
 
+var		index = 0;
 
 class DataManager {
 
@@ -30,7 +33,11 @@ class DataManager {
 			favorites: [],
 			visited: [],
 			downloaded: []
-		})
+		}).write();
+	}
+
+	commitDatabases() {
+		db.write();
 	}
 
 
@@ -38,18 +45,80 @@ class DataManager {
 	/*
 		Favorites
 	*/
-	addFavorite(u) {
+	addFavorite(e) {
+
+		db.get('favorites')
+			.push({
+				id: e.uid,
+				face: e.face,
+				nickname: e.nickname,
+				sex: e.sex,
+				level: e.level,
+				video_count: e.video_count,
+				usign: e.usign,
+				stars: e.stars
+			})
+			.write();
+
+		var list = db.get('favorites').cloneDeep().value();
+		this._emit('refresh_favorites', list);
+	}
+	loadFavorites() {
+		var list = db.get('favorites').cloneDeep().value();
+		this._emit('refresh_favorites', list);
+	}
+	removeFavorite(u) {
+		db.get('favorites').remove({ id: u.uid }).write();
+	}
+	updateFavorites() {
+		var count = db.get('favorites').size().value();
+		if (count == index) {
+			index = 0;
+		} 
+
+		index = 0;
+		this._updateFavorites();
+	
+	}
+	_updateFavorites() {
+		axios.get('http://live.ksmobile.net/user/getinfo',{
+			params: { 
+				userid: db.get('favorites['+index+'].id').value()
+			}
+		}).then(function(resp) {
+			var j = resp.data.data.user;
+
+			if (resp.data.status == 200) {
+				db.get('favorites').find({ id: j.user_info.uid })
+					.assign({ face: 	j.user_info.face })
+					.assign({ nickname: j.user_info.nickname })
+					.assign({ usign: 	j.user_info.usign })
+					.assign({ level: 	j.user_info.level })
+					.assign({ stars: 	j.user_info.stars })
+					.assign({ sex: 		j.user_info.sex > -1 ? ( j.user_info.sex > 0 ? 'male' : 'female') : '' })
+					.assign({ video_count: 	j.count_info.video_count })
+					.write();
+			} 
+
+		});
+		index++;
+
+		var count = db.get('favorites').size().value();
+		if (count == index) {
+			index = 0;
+			this.loadFavorites();
+		} else {
+			this._updateFavorites();
+		}
 
 	}
-	dropFavorite(u) {
 
+	isInFavorites(e) {
+		var t = db.get('favorites').find({ id: e }).value();
+		if (t == 'undefined' || typeof t == 'undefined' || t == null) return false; 
+		return true;
 	}
-	updateFavorite(u) {
 
-	}
-	isInFavorites(u) {
-
-	}
 
 	importFavorites(f) {
 
@@ -63,11 +132,21 @@ class DataManager {
 	/*
 		Tracking of Visited UserIds
 	*/
-	addTrackedVisited(u) {
-
+	addTrackedVisited(e) {
+		db.get('visited').push({
+			id: e,
+			dt: (new Date().getTime() / 1000) + 86400
+		}).write();
 	}
-	wasVisited(u) {
-
+	wasVisited(e) {
+		var dt = new Date().getTime() / 1000, t = db.get('visited').find({ id: e }).value();
+		if (t == 'undefined' || typeof t == 'undefined' || t == null) return false; 
+		if ((dt - t.dt) > 0) {
+			db.get('visited').remove({ id: e }).write();
+			return false;
+		} else {
+			return true;
+		}
 	}
 	expireVisited() {
 
